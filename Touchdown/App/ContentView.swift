a5ee4a69e11f5736cf06e2bce84dc9eb0ee14915
Nic Deane <njdeane7@gmail.com>
2021-09-22 19:20:43 +1000

    Create Brands section.

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -31,6 +31,17 @@ struct ContentView: View {
             
             TitleView(title: "Helmets")
             
+            LazyVGrid(columns: gridLayout, spacing: 15, content: {
+              ForEach(products) { product in
+                ProductItemView(product: product)
+              }
+            })
+            .padding(15)
+            
+            TitleView(title: "Brands")
+            
+            BrandGridView()
+            
             FooterView()
               .padding(.horizontal)
           } //: Vstack
---
+++ Touchdown/Model/BrandModel.swift
@@ -0,0 +1,13 @@
+//
+//  BrandModel.swift
+//  Touchdown
+//
+//  Created by Nic Deane on 22/9/21.
+//
+
+import Foundation
+
+struct Brand: Codable, Identifiable {
+  let id: Int
+  let image: String
+}
