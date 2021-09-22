058c0a3163ef81ac0bc9add79f5ae80dab523fef
Nic Deane <njdeane7@gmail.com>
2021-09-22 14:58:04 +1000

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Nic Deane on 22/9/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
