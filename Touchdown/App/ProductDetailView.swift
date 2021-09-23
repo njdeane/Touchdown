80c7dd0d78789a77530ed4c07cd4ab6d355b8c95
Nic Deane <njdeane7@gmail.com>
2021-09-23 19:32:39 +1000

    Finish layout of ProductDetailView.

--- Touchdown/App/ProductDetailView.swift
+++ Touchdown/App/ProductDetailView.swift
@@ -22,14 +22,39 @@ struct ProductDetailView: View {
       // Detail Top part
       TopPartDetailView()
         .padding(.horizontal)
+        .zIndex(1)
       
-      // detail button part
-      // Ratings + sizes
-      // Description
-      // Quantity + Favourite
-      // Add to cart
-      Spacer()
+      // Detail Bottom Part
+      VStack(alignment: .center, spacing: 0, content: {
+        // Ratings + sizes
+        RatingsSizesDetailView()
+          .padding(.top, -20)
+          .padding(.bottom, 10)
+        
+        // Description
+        ScrollView(.vertical, showsIndicators: false, content: {
+          Text(sampleProduct.description)
+            .font(.system(.body, design: .rounded))
+            .foregroundColor(.gray)
+            .multilineTextAlignment(.leading)
+        })
+        
+        // Quantity + Favourite
+        QuantityFavouriteDetailView()
+          .padding(.vertical, 10)
+        
+        // Add to cart
+        AddToCartDetailView()
+          .padding(.bottom, 20)
+      })
+      .padding(.horizontal)
+      .background(
+        Color.white.clipShape(CustomShape())
+          .padding(.top, -105)
+      )
+      
     })
+    .zIndex(0)
     .ignoresSafeArea(.all, edges: .all)
     .background(
       Color(
---
+++ Touchdown/View/Detail/AddToCartDetailView.swift
@@ -0,0 +1,37 @@
+//
+//  AddToCartDetailView.swift
+//  Touchdown
+//
+//  Created by Nic Deane on 23/9/21.
+//
+
+import SwiftUI
+
+struct AddToCartDetailView: View {
+  var body: some View {
+    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+      Spacer()
+      Text("Add To Cart".uppercased())
+        .font(.system(.title2, design: .rounded))
+        .fontWeight(.bold)
+        .foregroundColor(.white)
+      Spacer()
+    })
+    .padding(15)
+    .background(
+      Color(
+        red: sampleProduct.red,
+        green: sampleProduct.green,
+        blue: sampleProduct.blue)
+    )
+    .clipShape(Capsule())
+  }
+}
+
+struct AddToCartDetailView_Previews: PreviewProvider {
+  static var previews: some View {
+    AddToCartDetailView()
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
